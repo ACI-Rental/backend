63ae500171b48b967e58165eb754fbd77bc114d6
Cyrion <cyrion.vandongen@student.fontys.nl>
2021-11-04 09:55:09 +0100

    Add userspage compatibility

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -7,14 +7,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserService.Models.DTO;
 
 namespace UserService.Controllers
 {
     /// <summary>
     /// UserController this controller is used for managing the users in the ACI Rental system.
     /// </summary>
-    [Route("[controller]")]
     [ApiController]
+    [Route("[controller]")]
     public class UserController : ControllerBase
     {
         /// <summary>
@@ -38,8 +39,66 @@ namespace UserService.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            var result = await _dbContext.Users.ToListAsync();
+            var result = await _dbContext.Users.Include(u => u.Role).ToListAsync();
             return Ok(result);
         }
+
+        /// <summary>
+        /// Get all the Users for a single page
+        /// </summary>
+        /// <param name="pageIndex">Which page is being requested. If 0 or lower returns first page. If higher than amount of pages returns the last page </param>
+        /// <param name="pageSize">The amount of users that are being requested</param>
+        /// <returns>Object containing count of all users, current page and a collection of users</returns>
+        [HttpGet("page/{pageIndex}/{pageSize}")]
+        public async Task<IActionResult> GetUsersPage(int pageIndex, int pageSize)
+        {
+            if (pageIndex < 0)
+            {
+                return BadRequest("USERS.INCORRECT_INDEX");
+            }
+
+            if (pageSize < 0)
+            {
+                return BadRequest("USERS.INCORRECT_INDEX");
+            }
+
+            var page = new UsersPage();
+
+            var query = from user in _dbContext.Users
+                        orderby user.StudentNumber ascending
+                        select new User()
+                        {
+                            StudentNumber = user.StudentNumber,
+                            BannedUntil = user.BannedUntil,
+                            Role = user.Role
+                        };
+
+            page.TotalUserCount = await query.CountAsync();
+
+            // Last page calculation goes wrong if the totalcount is 0
+            // also no point in trying to get 0 products from DB
+            if (page.TotalUserCount == 0)
+            {
+                page.CurrentPage = 0;
+                page.Users = new List<User>(0);
+                return Ok(page);
+            }
+
+            // calculate how many pages there are given de current pageSize
+            int lastPage = (int)Math.Ceiling((double)page.TotalUserCount / pageSize) - 1;
+
+            // pageIndex below 0 is nonsensical, bringing the value to closest sane value
+            if (pageIndex < 0)
+            {
+                pageIndex = 0;
+            }
+
+            // use last page if requested page is higher
+            page.CurrentPage = Math.Min(pageIndex, lastPage);
+
+            page.Users = await (query).Skip(page.CurrentPage * pageSize).Take(pageSize).ToListAsync();
+            return Ok(page);
+        }
+
     }
 }
---
+++ UserService/Models/DTO/UsersPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Models.DTO
+{
+    /// <summary>
+    /// A page of users containing a subset of all users
+    /// </summary>
+    public class UsersPage
+    {
+        /// <summary>
+        /// The users contained with this page
+        /// </summary>
+        public IEnumerable<User> Users { get; set; }
+
+        /// <summary>
+        /// The current page number
+        /// Starts with 0
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// The total number of users
+        /// Not the number of users on the page
+        /// </summary>
+        public int TotalUserCount { get; set; }
+    
+    }
+}
