785510b4334e13dd9e6f9ab5eccf04464a639bbd
Bart Pelsma <Bart.pelsma@hotmail.com>
2021-12-08 14:10:46 +0100

    PDF service made

---
+++ PDFService/Models/DTO/CheckPdfModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDFService.Models.DTO
+{
+    public class CheckPdfModel
+    {
+        public string Pdf { get; set; }
+    }
+}
