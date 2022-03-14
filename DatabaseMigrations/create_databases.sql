f78f7605814571cc76cb376f1ede14ea3e9eb138
Stijn van Oudheusden <58070673+Stijnboss12@users.noreply.github.com>
2022-03-14 09:23:20 +0100

    Fix/working dev environment merge to s6 dev (#69)
    * add sql scripts and env variable
    
    * fix copy pasta issue in db string env
    
    * update seed script
    
    * fix port numbers
    
    * add user inserts
    
    Co-authored-by: Rachid <mail@rach.id>

---
+++ DatabaseMigrations/create_databases.sql
@@ -0,0 +1,10 @@
+create database [aci-note-service]
+go
+create database [aci-image-service]
+go
+create database [aci-reservation-service]
+go
+create database [aci-product-service]
+go
+create database [aci-user-service]
+go
