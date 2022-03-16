3acbeee4b1105f208efe79b3516a81d079421150
Stijn van Oudheusden <434876@student.fontys.nl>
2022-03-16 09:05:56 +0100

    add reservationrepository

---
+++ ACI.Reservations/Repositories/Interfaces/IReservationRepository.cs
@@ -0,0 +1,9 @@
+﻿using ACI.Reservations.Models;
+
+namespace ACI.Reservations.Repositories.Interfaces
+{
+    public interface IReservationRepository
+    {
+        public Task<List<Reservation>> GetReservations();
+    }
+}
---
+++ ACI.Reservations/Repositories/ReservationRepository.cs
@@ -0,0 +1,22 @@
+﻿using ACI.Reservations.DBContext;
+using ACI.Reservations.Models;
+using ACI.Reservations.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ACI.Reservations.Repositories
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        private readonly ReservationDBContext _dbContext;
+
+        public ReservationRepository(ReservationDBContext reservationDBContext)
+        {
+            _dbContext = reservationDBContext;
+        }
+
+        public async Task<List<Reservation>> GetReservations()
+        {
+            return await _dbContext.Reservations.ToListAsync();
+        }
+    }
+}
