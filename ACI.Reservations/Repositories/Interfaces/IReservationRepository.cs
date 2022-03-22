1f06ecb09ca80b9e98dc717b3143b248962416dc
Stijn van Oudheusden <434876@student.fontys.nl>
2022-03-22 15:41:28 +0100

    add methods to reservation repository

--- ACI.Reservations/Repositories/Interfaces/IReservationRepository.cs
+++ ACI.Reservations/Repositories/Interfaces/IReservationRepository.cs
@@ -1,11 +1,18 @@
-﻿using ACI.Reservations.Models;
+﻿using ACI.Reservations.Domain;
+using ACI.Reservations.Models;
+using LanguageExt;
 
 namespace ACI.Reservations.Repositories.Interfaces
 {
     public interface IReservationRepository
     {
-        public Task<List<Reservation>> GetReservations();
-        public Task<List<Reservation>> GetReservationsByStartDate(DateTime startDate);
-        public Task<List<Reservation>> GetReservationsByEndDate(DateTime endDate);
+        public Task<Either<IError, List<Reservation>>> GetReservations();
+        public Task<Either<IError, List<Reservation>>> GetReservationsByStartDate(DateTime startDate);
+        public Task<Either<IError, List<Reservation>>> GetReservationsByEndDate(DateTime endDate);
+        public Task<Either<IError, List<Reservation>>> GetReservationsByProductId(Guid productId);
+        public Task<Either<IError, Reservation>> GetReservationByReservationId(Guid reservationId);
+        public Task<Either<IError, Reservation>> GetOverlappingReservation(Guid productId, DateTime startDate, DateTime endDate);
+        public Task<Either<IError, Reservation>> UpdateReservation(Reservation reservation);
+        public Task<Either<IError, Reservation>> CreateReservation(Reservation reservation);
     }
 }
--- ACI.Reservations/Repositories/ReservationRepository.cs
+++ ACI.Reservations/Repositories/ReservationRepository.cs
@@ -1,6 +1,9 @@
 ﻿using ACI.Reservations.DBContext;
+using ACI.Reservations.Domain;
 using ACI.Reservations.Models;
 using ACI.Reservations.Repositories.Interfaces;
+using LanguageExt;
+using LanguageExt.UnsafeValueAccess;
 using Microsoft.EntityFrameworkCore;
 
 namespace ACI.Reservations.Repositories
@@ -14,19 +17,106 @@ namespace ACI.Reservations.Repositories
             _dbContext = reservationDBContext;
         }
 
-        public async Task<List<Reservation>> GetReservations()
+        public async Task<Either<IError, List<Reservation>>> GetReservations()
         {
-            return await _dbContext.Reservations.ToListAsync();
+            var result = await _dbContext.Reservations.ToListAsync() ?? Option<List<Reservation>>.None;
+
+            if (result == Option<List<Reservation>>.None)
+            {
+                return AppErrors.FailedToFindReservation;
+            }
+
+            return result.ValueUnsafe();
         }
 
-        public async Task<List<Reservation>> GetReservationsByStartDate(DateTime startDate)
+        public async Task<Either<IError, List<Reservation>>> GetReservationsByStartDate(DateTime startDate)
         {
-            return await _dbContext.Reservations.Where(x => x.StartDate.Date == startDate.Date).ToListAsync();
+            var result = await _dbContext.Reservations.Where(x => x.StartDate.Date == startDate.Date).ToListAsync() ?? Option<List<Reservation>>.None;
+
+            if (result == Option<List<Reservation>>.None)
+            {
+                return AppErrors.FailedToFindReservation;
+            }
+
+            return result.ValueUnsafe();
         }
 
-        public async Task<List<Reservation>> GetReservationsByEndDate(DateTime endDate)
+        public async Task<Either<IError, List<Reservation>>> GetReservationsByEndDate(DateTime endDate)
         {
-            return await _dbContext.Reservations.Where(x => x.EndDate.Date == endDate.Date).ToListAsync();
+            var result = await _dbContext.Reservations.Where(x => x.EndDate.Date == endDate.Date).ToListAsync() ?? Option<List<Reservation>>.None;
+
+            if (result == Option<List<Reservation>>.None)
+            {
+                return AppErrors.FailedToFindReservation;
+            }
+
+            return result.ValueUnsafe();
+        }
+
+        public async Task<Either<IError, List<Reservation>>> GetReservationsByProductId(Guid productId)
+        {
+            var result = await _dbContext.Reservations.Where(x => x.ProductId == productId).ToListAsync() ?? Option<List<Reservation>>.None;
+
+            if (result == Option<List<Reservation>>.None)
+            {
+                return AppErrors.FailedToFindReservation;
+            }
+
+            return result.ValueUnsafe();
+        }
+
+        public async Task<Either<IError, Reservation>> GetReservationByReservationId(Guid reservationId)
+        {
+            var result = await _dbContext.Reservations.Where(x => x.Id == reservationId).FirstOrDefaultAsync() ?? Option<Reservation>.None;
+
+            if (result == Option<Reservation>.None)
+            {
+                return AppErrors.FailedToFindReservation;
+            }
+
+            return result.ValueUnsafe();
+        }
+
+        public async Task<Either<IError, Reservation>> GetOverlappingReservation(Guid productId, DateTime startDate, DateTime endDate)
+        {
+            var result = await _dbContext.Reservations.Where(x => x.ProductId == productId && x.StartDate <= endDate && startDate < x.EndDate).FirstOrDefaultAsync() ?? Option<Reservation>.None;
+
+            if (result == Option<Reservation>.None)
+            {
+                return AppErrors.FailedToFindReservation;
+            }
+
+            return result.ValueUnsafe();
+        }
+
+        public async Task<Either<IError, Reservation>> UpdateReservation(Reservation reservation)
+        {
+            var reservationToUpdate = await _dbContext.Reservations.Where(x => x.Id == reservation.Id).FirstOrDefaultAsync() ?? Option<Reservation>.None;
+
+            if (reservationToUpdate == Option<Reservation>.None)
+            {
+                return AppErrors.FailedToFindReservation;
+            }
+
+            reservationToUpdate = reservation;
+
+            if (_dbContext.SaveChangesAsync().IsCompletedSuccessfully)
+            {
+                return reservationToUpdate.ValueUnsafe();
+            }
+
+            return AppErrors.FailedToSaveReservation;
+        }
+
+        public async Task<Either<IError, Reservation>> CreateReservation(Reservation reservation)
+        {
+            await _dbContext.Reservations.AddAsync(reservation);
+            if (_dbContext.SaveChangesAsync().IsCompletedSuccessfully)
+            {
+                return reservation;
+            }
+
+            return AppErrors.FailedToSaveReservation;
         }
     }
 }
