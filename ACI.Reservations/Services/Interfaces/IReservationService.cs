62127b660edc9103bd72ec6677f0bb1bcffee8a1
Stijn van Oudheusden <434876@student.fontys.nl>
2022-03-22 15:41:34 +0100

    add reservationservice

--- ACI.Reservations/Services/Interfaces/IReservationService.cs
+++ ACI.Reservations/Services/Interfaces/IReservationService.cs
@@ -1,4 +1,7 @@
-﻿using ACI.Reservations.Models;
+﻿using ACI.Reservations.Domain;
+using ACI.Reservations.Models;
+using ACI.Reservations.Models.DTO;
+using LanguageExt;
 
 namespace ACI.Reservations.Services.Interfaces
 {
@@ -7,5 +10,8 @@ namespace ACI.Reservations.Services.Interfaces
         public Task<List<Reservation>> GetReservations();
         public Task<List<Reservation>> GetReservationsByStartDate(DateTime startDate);
         public Task<List<Reservation>> GetReservationsByEndDate(DateTime endDate);
+        public Task<List<Reservation>> GetReservationsByProductId(Guid productId);
+        public Task<Either<IError, Reservation>> ExecuteReservationAction(Guid reservationId, ReservationAction action);
+        public Task<Either<IError, Reservation>> ReserveProduct(ProductReservationDTO productReservationDTO);
     }
 }
