cbd48e3003bdeda8c3768b68d8f38f92b695e8a7
Rachid <mail@rach.id>
2022-03-23 01:46:04 +0100

    test 2

--- ACI.Products/Program.cs
+++ ACI.Products/Program.cs
@@ -27,7 +27,7 @@ finally
     Log.CloseAndFlush();
 }
 
-void Run()
+void Run( )
 {
     var builder = WebApplication.CreateBuilder(args);
 
@@ -68,7 +68,6 @@ void Run()
         var context = services.GetRequiredService<ProductContext>();
         context.Database.EnsureCreated();
     }
-
 
     app.UseHttpsRedirection();
 
