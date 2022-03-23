2be37bb54c943d4b3c19a1d7ce381e7896684aca
Noodleyy <jon71cool@hotmail.com>
2022-03-23 09:31:00 +0100

    WIP

--- ACI.Images/Data/Repositories/ImageRepository.cs
+++ ACI.Images/Data/Repositories/ImageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using ACI.ImageService.Data.Repositories.Interfaces;
 using ACI.ImageService.Domain;
@@ -19,17 +20,18 @@ namespace ACI.ImageService.Data.Repositories
         
         public ImageRepository(IConfiguration configuration, ILogger<ImageRepository> logger)
         {
-            string test = configuration["AzureBlobStorage:ConnectionStrings:Azurite"];
-            _blobContainerClient = new BlobContainerClient("DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=https://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=https://127.0.0.1:10001/devstoreaccount1;TableEndpoint=https://127.0.0.1:10002/devstoreaccount1;", "yeppers1");
+            _blobContainerClient = new BlobContainerClient(configuration["AzureBlobStorage:ConnectionStrings:Azurite"], configuration["AzureBlobStorage:Containers:ProductImages"]);
             _blobContainerClient.CreateIfNotExists();
             _blobServiceClient = new BlobServiceClient(configuration["AzureBlobStorage:ConnectionStrings:Azurite"]);
             _logger = logger;
-
         }
+        
         public async Task<Either<IError, ProductImageBlob>> AddProductImageBlob(ProductImageBlob productImageBlob)
         {
-            string filepath = "C:/Users/jonah/Pictures/AnonWorld/Sample/PeepoWeird.png";
-            BlobClient blob = _blobContainerClient.GetBlobClient("henk");
+            string filepath = "C:/Users/jonah/Pictures/how-it-looks-when-someone-with-an-android-accidentally-turns-18866052.png";
+
+            var ext = Path.GetExtension(filepath);
+            BlobClient blob = _blobContainerClient.GetBlobClient("henk.png");
             await blob.UploadAsync(filepath);
             throw new NotImplementedException("cock??");
         }
