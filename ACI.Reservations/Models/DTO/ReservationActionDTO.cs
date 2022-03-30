caacefb517eb75a682676ef13013ebc446c598c1
Stijn van Oudheusden <58070673+Stijnboss12@users.noreply.github.com>
2022-03-30 11:30:42 +0200

    Feature/refactor reservation service (#79)
    * add sql scripts and env variable
    
    * fix copy pasta issue in db string env
    
    * update seed script
    
    * fix port numbers
    
    * add user inserts
    
    * adds basic api setup
    
    * adds shared project, with logging / versioning
    
    * fix comment
    
    * include nuget packages
    
    * add line
    
    * setup project
    
    * add logging and database to project
    
    * add controller
    
    * add dbcontext
    
    * add reservation class
    
    * add logging and services to program
    
    * setup project
    
    * add directory for services and interfaces
    
    * add reservationrepository
    
    * add reservationservice
    
    * add dependency injection
    
    * copy code to controller to rewrite
    
    * csproj file change
    
    * add new endpoints for similar start or end date requests
    
    * add methods to get reservations by start- or enddate
    
    * add methods to get reservations by start- or enddate
    
    * move null check to service and add count check
    
    * change to scoped
    
    * add errors to application
    
    * update  project
    
    * add endpoints
    
    * add product models
    
    * add reservation models
    
    * update program
    
    * add methods to reservation repository
    
    * add reservationservice
    
    * fix endpoints
    
    * update methods in reservationservice
    
    * remove httpclient dependency injection
    
    * add reservationactiondto
    
    * update endpoints
    
    Co-authored-by: Rachid <mail@rach.id>

---
+++ ACI.Reservations/Models/DTO/ReservationActionDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ACI.Reservations.Models.DTO
+{
+    public class ReservationActionDTO
+    {
+        [Required]
+        public Guid ReservationId { get; set; }
+
+        [Required]
+        public int ReservationAction { get; set; }
+    }
+}
