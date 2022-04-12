dc3df93c657a63e17a6d8149b2594d2184732cfc
Stijn van Oudheusden <434876@student.fontys.nl>
2022-04-12 14:55:15 +0200

    add appfactory

---
+++ ACI.Reservations.Test.Integration/Fixtures/ReservationAppFactory.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACI.Reservations.Test.Integration.Fixtures
+{
+    public class ReservationAppFactory : WebApplicationFactory<Program>
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.ConfigureServices(serviceCollection =>
+            {
+                serviceCollection.UseTestDatabase();
+            });
+
+            builder.ConfigureAppConfiguration((_, configurationBuilder) =>
+            {
+                configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+
+                // Add settings from appsettings.json in integration testing project
+                var projectDir = Directory.GetCurrentDirectory();
+                var configPath = Path.Combine(projectDir, "appsettings.json");
+                configurationBuilder.AddJsonFile(configPath, optional: false);
+
+                // Add user secrets in API project
+                configurationBuilder.AddUserSecrets<Program>(optional: true);
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
