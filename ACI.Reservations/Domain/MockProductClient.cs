9b4d50c45d4b4192ab8758f5d33207792ec6c7cb
Stijn van Oudheusden <434876@student.fontys.nl>
2022-04-12 15:02:57 +0200

    comment method

--- ACI.Reservations/Domain/MockProductClient.cs
+++ ACI.Reservations/Domain/MockProductClient.cs
@@ -3,21 +3,21 @@ using LanguageExt;
 
 namespace ACI.Reservations.Domain
 {
-    public class MockProductClient : IProductClient
+    public class MockProductClient //: IProductClient
     {
-        public async Task<Either<IError, ProductDTO>> GetProduct(Guid productId)
-        {
-            var productDTO = new ProductDTO()
-            {
-                Id = Guid.Parse("08bc0982-49ef-469b-a276-f9e515586481"),
-                Name = "Camera",
-                Description = "Mooie Camera",
-                RequiresApproval = false,
-                IsDeleted = false,
-                CategoryId = 1,
-            };
+        //public async Task<Either<IError, ProductDTO>> GetProduct(Guid productId)
+        //{
+        //    var productDTO = new ProductDTO()
+        //    {
+        //        Id = Guid.Parse("08bc0982-49ef-469b-a276-f9e515586481"),
+        //        Name = "Camera",
+        //        Description = "Mooie Camera",
+        //        RequiresApproval = false,
+        //        IsDeleted = false,
+        //        CategoryId = 1,
+        //    };
 
-            return productDTO;
-        }
+        //    return productDTO;
+        //}
     }
 }
