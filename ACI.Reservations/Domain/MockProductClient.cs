b4c0ae909e2681183c66baaba070c81bff37f41b
Stijn van Oudheusden <434876@student.fontys.nl>
2022-04-12 14:56:25 +0200

    add mockproductclient for testing purposes

---
+++ ACI.Reservations/Domain/MockProductClient.cs
@@ -0,0 +1,23 @@
+﻿using ACI.Reservations.Models;
+using LanguageExt;
+
+namespace ACI.Reservations.Domain
+{
+    public class MockProductClient : IProductClient
+    {
+        public async Task<Either<IError, ProductDTO>> GetProduct(Guid productId)
+        {
+            var productDTO = new ProductDTO()
+            {
+                Id = Guid.Parse("08bc0982-49ef-469b-a276-f9e515586481"),
+                Name = "Camera",
+                Description = "Mooie Camera",
+                RequiresApproval = false,
+                IsDeleted = false,
+                CategoryId = 1,
+            };
+
+            return productDTO;
+        }
+    }
+}
