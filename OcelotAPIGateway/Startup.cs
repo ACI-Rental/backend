4e27ba68a3210546f33eadcae9e84d6fdc6311a8
Rachid <mail@rach.id>
2022-05-24 11:48:35 +0200

    Remove Auth0

--- OcelotAPIGateway/Startup.cs
+++ OcelotAPIGateway/Startup.cs
@@ -1,11 +1,8 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -24,30 +21,7 @@ namespace OcelotAPIGateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            string domain = Configuration["Auth0:Domain"];
-            const string authenticationProviderKey = "Auth0";
-
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                
-            }).AddJwtBearer(authenticationProviderKey, options =>
-            {
-                options.Authority = domain;
-                options.Audience = Configuration["Auth0:Audience"];
-            });
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("read:users", policy => policy.Requirements.Add(new HasScopeRequirement("read:users", domain)));
-            });
-            
             services.AddOcelot();
-            
-            services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
