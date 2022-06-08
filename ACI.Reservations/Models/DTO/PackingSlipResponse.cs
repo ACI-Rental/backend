81554b608e19d1d9a01781563976a41bcc49a8fb
Noodleyy <jon71cool@gmail.com>
2022-06-08 10:46:15 +0200

    Add packingslipresponse

---
+++ ACI.Reservations/Models/DTO/PackingSlipResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ACI.Reservations.Models.DTO;
+
+public class PackingSlipResponse
+{
+
+    public Guid Id { get; }
+    public DateTime StartDate { get; }
+    public DateTime EndDate { get; }
+    public string RenterId { get; }
+    public Guid ProductId { get; }
+    public string ProductName { get; }
+
+    public PackingSlipResponse(Guid id, DateTime startDate, DateTime endDate, string renterId, Guid productId, string productName)
+    {
+        Id = id;
+        StartDate = startDate;
+        EndDate = endDate;
+        RenterId = renterId;
+        ProductId = productId;
+        ProductName = productName;
+    }
+    
+    public static PackingSlipResponse MapFromModel(Reservation model)
+    => new(
+        model.Id,
+        model.StartDate,
+        model.EndDate,
+        model.RenterId,
+        model.ProductId,
+        model.RentedProduct.Name
+    );
+
+}
